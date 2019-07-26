8dc0c4c358481859d22efa0484324004eec11651
Mohammad Firoj Haider <myappworld114@gmail.com>
2019-07-25 22:34:24 -0400

    Added dtog yaml config.

--- src/main/java/org/mfh144/dtog/DtogApp.java
+++ src/main/java/org/mfh144/dtog/DtogApp.java
@@ -1,13 +1,32 @@
 package org.mfh144.dtog;
 
+import org.mfh144.dtog.config.DtogConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * Main class of the DTOG application. 
+ * Main class of the DTOG application.
  *
  */
-public class DtogApp 
-{
-    public static void main( String[] args )
-    {
-        
-    }
+@SpringBootApplication
+public class DtogApp implements CommandLineRunner {
+
+	@Autowired
+	private DtogConfig dtogConfig;
+
+	public static void main(String[] args) {
+
+		SpringApplication application = new SpringApplication(DtogApp.class);
+		application.run(args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("DtogConfig: " + dtogConfig.toString());
+
+	}
+
 }
---
+++ src/main/java/org/mfh144/dtog/config/DtogConfig.java
@@ -0,0 +1,29 @@
+package org.mfh144.dtog.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties
+public class DtogConfig {
+
+	private Application application;
+
+	public Application getApplication() {
+		return application;
+	}
+
+	public void setApplication(Application application) {
+		this.application = application;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("DtogConfig [application=");
+		builder.append(application);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
